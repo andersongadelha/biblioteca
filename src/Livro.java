067f9bcb15b53dd6e495b5769d8c24f855a94df3
Anderson Lima <anderson.gadelha@zup.com.br>
2024-12-18 08:56:16 -0300

    feat: Adiciona métodos da classe livro

--- src/Livro.java
+++ src/Livro.java
@@ -3,4 +3,27 @@ public class Livro {
     private String autor;
     private String isbn;
     private boolean disponivel;
+
+    public Livro(String titulo, String autor, String isbn, boolean disponivel) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.disponivel = disponivel;
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Isbn: " + isbn);
+        System.out.println("Disponível: " + disponivel);
+    }
+
+    public void emprestar() {
+        this.disponivel = false;
+    }
+
+    public void devolver() {
+        this.disponivel = true;
+    }
+    
 }
